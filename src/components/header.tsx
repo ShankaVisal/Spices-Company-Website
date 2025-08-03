96d8d1e5d4180ad42721755888afbba07cd1b808
Shanka Visal <shankavisal@gmail.com>
2025-08-04 01:59:20 +0530

    update the websites

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -30,7 +30,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
         <Link href="/" className="mr-auto flex items-center gap-2">
-          <Image src={logo} alt="Devi Products Logo" width={80} height={80} data-ai-hint="company logo" />
+          <Image src={logo} alt="Devi Products Logo" width={40} height={40} data-ai-hint="company logo" />
           <span className="font-headline text-2xl font-bold">
             {uiStrings.deviProducts[language]}
           </span>
